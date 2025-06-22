37a8a07f03b8a6a513751235eba0b04204cdbb92
ayushpatil2122 <ayushitkhede4@gmail.com>
2025-06-22 20:48:39 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
 import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
@@ -9,6 +8,7 @@ import NutritionSection from "./sections/NutritionSection";
 import BenefitSection from "./sections/BenefitSection";
 import TestimonialSection from "./sections/TestimonialSection";
 import FooterSection from "./sections/FooterSection";
+import Navbar from "./components/NavBar";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -22,7 +22,7 @@ const App = () => {
 
   return (
     <main>
-      <NavBar />
+      <Navbar />
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <HeroSection />
